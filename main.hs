f018467f687510567f1a1059c7939f8f9381e21e
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2022-01-26 22:30:57 +0000

    fun list stuff

--- main.hs
+++ main.hs
@@ -17,9 +17,20 @@ first3Primes = take 3 morePrimes2
 removedPrimes = drop 3 morePrimes2
 is7InList = 7 `elem` morePrimes2
 maxPrime = maximum morePrimes2
+minPrime = minimum morePrimes2
+totalPrime = sum morePrimes2
 
+newList = [2,3,5]
+prodPrimes = product newList
 
+zeroToTen = [0..10]
 
+evenList = [2,4..20]
+letterList = ['A', 'C'..'Z']
+
+infiniteList10 = [10,20..]
+
+listOfTwo = take 10 (repeat 2)
 
 
 
